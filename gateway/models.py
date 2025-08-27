6ddb83dd39aa04fc8d9694989717e323a3a9e0cb
IACFO <iafinanceirozap@gmail.com>
2025-08-27 17:32:13 -0300

    Update models.py

--- gateway/models.py
+++ gateway/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import Integer, String, JSON, DateTime, text
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, JSON, DateTime, text, ForeignKey
 
 Base = declarative_base()
 
@@ -10,3 +10,13 @@ class User(Base):
     email: Mapped[str] = mapped_column(String, index=True)
     created_at: Mapped[str] = mapped_column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
     access_expires_at: Mapped[DateTime] = mapped_column(DateTime, nullable=True)  # NOVO
+
+    store = relationship("Store", back_populates="user", uselist=False)
+
+class Store(Base):
+    __tablename__ = "store"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
+    data: Mapped[dict] = mapped_column(JSON, nullable=False)
+
+    user = relationship("User", back_populates="store")
